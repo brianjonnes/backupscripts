f2e533ce369442992bb6ebf05496a54bb763795b
U-4720S-BRIAN\brian <brianj@technews.co.za>
2020-07-02 20:52:06 +0200

    amends

--- 0-backup.cmd
+++ 0-backup.cmd
@@ -5,6 +5,12 @@ setlocal enabledelayedexpansion
 call check_disk.cmd
 if errorlevel 1 echo "no backup disk found" && goto :end
 
+if "%COMPUTERNAME%" == "4720S-BRIAN" goto :4720s
+echo computer "%COMPUTERNAME%" not valid
+goto :end
+
+:4720s
+
 rem for %%m in (f g h) do (
 rem 	if exist %%m:\media-name (
 rem 		set MEDIA=%%m:
@@ -17,7 +23,7 @@ rem )
 rem echo "no disk found"
 rem goto :end
 
-:run2
+rem :run2
 echo backing up to %w% on %MEDIA%
 if "%1" == "clean" ( set rcopt=/mir ) else ( set rcopt=/e )
 robocopy %rcopt% c:\users\brian\documents %MEDIA%\hp4720s-documents /xf desktop.ini /xd brian-misc.git /xd not-backed-up from-usb-a projects share "My Music" "My Pictures" "My Videos" 
