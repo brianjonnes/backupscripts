8be0240c85ea074a5a225545836f714f37383228
unknown <brian@init_systems_2>
2020-07-25 15:06:07 +0200

    amends

--- 0-backup-dns323-documents.cmd
+++ 0-backup-dns323-documents.cmd
@@ -6,8 +6,16 @@ call check_disk.cmd
 if errorlevel 1 echo "no backup disk found" && goto :end
 
 :run2
+choice /c DALC /m "D - Remove deleted, A - Ignore extra files, L - Dry-run, C - Cancel:"
+if %errorlevel% equ 1 ( set rcopt=/mir && goto :run3 )
+if %errorlevel% equ 2 ( set rcopt=/e && goto :run3 )
+if %errorlevel% equ 3 ( set "rcopt=/l /e" && goto :run3 )
+echo No choice made.
+goto :end
+
+:run3
 echo backing up to %w% on %MEDIA%
-if "%1" == "clean" ( set rcopt=/mir ) else ( set rcopt=/e )
+rem if "%1" == "clean" ( set rcopt=/mir ) else ( set rcopt=/e )
 
 robocopy %rcopt% \\dns323\volume_1\documents %MEDIA%\dns323-documents
 echo completed backing up to %w% on %MEDIA%
--- 0-backup-tl500-shipyard.cmd
+++ 0-backup-tl500-shipyard.cmd
@@ -5,7 +5,11 @@ setlocal enabledelayedexpansion
 call check_disk.cmd
 if errorlevel 1 echo "no backup disk found" && goto :end
 
-:run2
+if "%COMPUTERNAME%" == "DESKTOP-JD81E61" goto :tl500
+echo computer "%COMPUTERNAME%" not valid
+goto :end
+
+:tl500
 choice /c DALC /m "D - Remove deleted, A - Ignore extra files, L - Dry-run, C - Cancel:"
 if %errorlevel% equ 1 ( set rcopt=/mir && goto :run3 )
 if %errorlevel% equ 2 ( set rcopt=/e && goto :run3 )
@@ -15,8 +19,7 @@ goto :end
 
 :run3
 echo backing up to %w% on %MEDIA%
-
-robocopy %rcopt% \\dns323\volume_1\projects %MEDIA%\dns323-projects
+robocopy %rcopt% c:\shipyard %MEDIA%\tl500-shipyard /x /xf *.obj *.dll *.exe *.exp *.lib 
 echo completed backing up to %w% on %MEDIA%
 
 :end
