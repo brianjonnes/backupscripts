ce5b701beb3122a34e16844ac90dad9d1cd6009f
U-4720S-BRIAN\brian <brianj@technews.co.za>
2020-08-16 21:10:20 +0200

    amends

--- 0-backup-4720s-documents.cmd
+++ 0-backup-4720s-documents.cmd
@@ -10,10 +10,11 @@ echo computer "%COMPUTERNAME%" not valid
 goto :end
 
 :4720s
-choice /c DALC /m "D - Remove deleted, A - Ignore extra files, L - Dry-run, C - Cancel:"
-if %errorlevel% equ 1 ( set rcopt=/mir && goto :run3 )
-if %errorlevel% equ 2 ( set rcopt=/e && goto :run3 )
-if %errorlevel% equ 3 ( set "rcopt=/l /e" && goto :run3 )
+choice /c DALFC /m "D - Remove deleted, A - Ignore extra files, L - Dry-run, F - Dry-run 2, C - Cancel:"
+if %errorlevel% equ 1 ( set "rcopt=/mir /ndl" && goto :run3 )
+if %errorlevel% equ 2 ( set "rcopt=/e /ndl" && goto :run3 )
+if %errorlevel% equ 3 ( set "rcopt=/l /e /ndl" && goto :run3 )
+if %errorlevel% equ 4 ( set "rcopt=/l /e" && goto :run3 )
 echo No choice made.
 goto :end
 
--- 0-backup-dns323-documents.cmd
+++ 0-backup-dns323-documents.cmd
@@ -6,10 +6,11 @@ call check_disk.cmd
 if errorlevel 1 echo "no backup disk found" && goto :end
 
 :run2
-choice /c DALC /m "D - Remove deleted, A - Ignore extra files, L - Dry-run, C - Cancel:"
-if %errorlevel% equ 1 ( set rcopt=/mir && goto :run3 )
-if %errorlevel% equ 2 ( set rcopt=/e && goto :run3 )
-if %errorlevel% equ 3 ( set "rcopt=/l /e" && goto :run3 )
+choice /c DALFC /m "D - Remove deleted, A - Ignore extra files, L - Dry-run, F - Dry-run 2, C - Cancel:"
+if %errorlevel% equ 1 ( set "rcopt=/mir /ndl" && goto :run3 )
+if %errorlevel% equ 2 ( set "rcopt=/e /ndl" && goto :run3 )
+if %errorlevel% equ 3 ( set "rcopt=/l /e /ndl" && goto :run3 )
+if %errorlevel% equ 4 ( set "rcopt=/l /e" && goto :run3 )
 echo No choice made.
 goto :end
 
